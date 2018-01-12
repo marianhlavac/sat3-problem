0aac58d264170712f2ac541107bb3fbe737020a0
Marián Hlaváč <hlavac95@gmail.com>
2018-01-12 19:59:34 +0100

    Add uniform crossover operator

--- src/genetic/crossover.rs
+++ src/genetic/crossover.rs
@@ -1,4 +1,5 @@
 use rand::{thread_rng, Rng};
+use genetic::utils::{odds_are};
 
 pub fn single_point(p1: Vec<bool>, p2: Vec<bool>) -> Vec<bool> {
     let size: usize = p1.len();
@@ -9,5 +10,16 @@ pub fn single_point(p1: Vec<bool>, p2: Vec<bool>) -> Vec<bool> {
         child[i] = if i <= midpoint { p1[i] } else { p2[i] }; 
     }
     
+    child
+}
+
+pub fn uniform(p1: Vec<bool>, p2: Vec<bool>, ratio: f32) -> Vec<bool> {
+    let size: usize = p1.len();
+    let mut child: Vec<bool> = vec![false; size];
+
+    for i in 0..size {
+        child[i] = if odds_are(ratio) { p1[i] } else { p2[i] };
+    }
+
     child
 }
